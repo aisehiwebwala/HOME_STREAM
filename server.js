7254ff56c78e4105b83c54e4f4d12e4be782965d
aisehiwebwala <116726737+aisehiwebwala@users.noreply.github.com>
2024-04-05 00:14:54 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,11 +1,11 @@
 const express = require("express");
 const app = express();
-
+const cors = require("cors");
 const port = process.env.port | 3000;
 
 const urlRoute = require("./src/urlRoutes");
 const imgRoute = require("./src/imgRoutes");
-
+app.use(cors());
 app.use(express.json());
 app.use("/url", urlRoute);
 app.use("/img", imgRoute);
